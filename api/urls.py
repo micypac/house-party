d557f44a982f2bcf80e301f944d8fc8fb5b9e637
Michael Pacleb <michael.pacleb@gmail.com>
2023-04-10 18:02:17 -0400

    api backend: created new url route 'get-room' and RoomDetail view to get the information details of specific room by providing the code as query params.

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import RoomList, CreateRoomView
+from .views import RoomList, CreateRoomView, RoomDetail
 
 urlpatterns = [
     path("room", RoomList.as_view(), name="room_list"),
     path("create-room", CreateRoomView.as_view(), name="room_create"),
+    path("get-room", RoomDetail.as_view(), name="room_detail"),
 ]
